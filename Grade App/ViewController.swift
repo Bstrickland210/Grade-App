c5000d2fff04187c1e65659e54606e030cf9c783
Bridger Strickland <bridger210strick@gmail.com>
2021-10-13 10:18:08 -0600

    MVP

--- Grade App/ViewController.swift
+++ Grade App/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var wantedGrade: UITextField!
+    
+    @IBOutlet weak var currentGrade: UITextField!
+    
+    @IBOutlet weak var weightFinal: UITextField!
+    
+    @IBOutlet weak var myLabel: UITextField!
+    
+    var name = ""
+    
+    var myInt1 = 0.0
+    
+    var Name = ""
+    
+    var myInt2 = 0.0
+    
+    var nAme = ""
+    
+    var myInt3 = 0.0
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateButton(_ sender: Any)
+    {
+        name = wantedGrade.text ?? "0.0"
+        myInt1 = Double(name) ?? 0.0
+        
+        Name = currentGrade.text ?? "0.0"
+        myInt2 = Double(Name) ?? 0.0
+        
+        nAme = weightFinal.text ?? "0.0"
+        myInt3 = Double(nAme) ?? 0.0
+        
+       var F = (myInt1-myInt2*(1-myInt3/100))/(myInt3/100)
+        myLabel.text = "\(F)"
+    }
+        
 }
 
